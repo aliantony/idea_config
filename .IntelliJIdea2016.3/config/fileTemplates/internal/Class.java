0d3d65a728f7af92666edf4d28b1ca30f4b36c81
王谦 <wangqian-0@thunisoft.com>
2018-02-09 14:28:58 +0800

    idea配置信息提交

---
+++ .IntelliJIdea2016.3/config/fileTemplates/internal/Class.java
@@ -0,0 +1,9 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+
+/**
+*   ${Description}
+*   @Author wangqian
+*   Created on ${YEAR}-${MONTH}-${DAY} ${DATE}
+*/
+public class ${NAME} {
+}
